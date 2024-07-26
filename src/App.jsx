d82b3856cd4cae273eb44f7692180bab8c4e35d1
LenaDurant <durant.arlena@gmail.com>
2024-07-26 10:39:49 -0400

    finished tab two and half of tab three code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-
+import { Routes, Route, Link } from "react-router-dom"
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+    <div id="container">
+      <h1></h1>
+      <div id="navbar">
+        <Link to="/blue" element={<h1>Blue</h1>} />
+        <Link to="/red" element={<h1>Red</h1>} />
+      </div>
+      <div id="main-section">{/* routes here */}
+        <Routes>
+          <Route path="/blue" element={<h1>Blue</h1>} />
+          <Route path="/red" element={<h1>Red</h1>} />
+        </Routes>
+      </div>
     </div>
     </>
   )
---
+++ src/Blue.jsx
@@ -0,0 +1,7 @@
+export default function Blue() {
+    return (
+      <div className="red">
+        <h1>Blue</h1>
+      </div>
+    );
+  }
